6e3c8ccd7a828e4c3277a8f25df64d408362a426
UrkiAristu <urki.aristu@unavarra.es>
2026-05-05 15:19:12 +0200

    Eliminados bundles de CSS, RTL y dark mode no utilizados

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import purgecss from 'vite-plugin-purgecss';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +8,14 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
+        purgecss({
+            content: [
+                './resources/**/*.blade.php', // Que busque las clases en tus vistas
+                './resources/**/*.js',        // Y en tus scripts
+                './resources/**/*.vue'
+            ],
+            // safelist: Evita que borre clases dinámicas de Bootstrap (ej: modales o menús desplegables)
+            safelist: [/^modal-/, /^fade/, /^show/, /^collapse/, /^dropdown-/, /^nav-/]
+        })
     ],
 });
